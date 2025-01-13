c42c9ddeff0b3908bed2ac9823f52d907e0c8b54
turinglabs <sebastiano.cataudo@gmail.com>
2025-01-13 12:10:19 +0100

    init commit

---
+++ test/HelloWorld.t.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Test, console} from "forge-std/Test.sol";
+import {HelloWorld} from "../src/HelloWorld.sol";
+
+contract HelloWorldTest is Test {
+    HelloWorld public helloWorld;
+
+    function setUp() public {
+        helloWorld = new HelloWorld();
+    }
+
+    function test_Hello() public view {
+        assertEq(helloWorld.hello(), "Hello World");
+    }
+}
